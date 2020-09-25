e730cb92c51ba6e2e3779bfebcdbf6c87f850217
Akash Bhatt <31155925+Akashbhatt143@users.noreply.github.com>
2020-09-25 17:53:30 +0530

    Added new scenes
    - Plane Detection Scene
    - Object Placement Scene

---
+++ Assets/Scenes/Object Placement/PlacementManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlacementManager : MonoBehaviour
+{
+    public GameObject placedPrefab;
+    private ARRaycastManager arRaycastManager;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                if (arRaycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+                {
+                    var hitPose = hits[0].pose;
+                    Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+                }
+            }
+        }
+    }
+}
